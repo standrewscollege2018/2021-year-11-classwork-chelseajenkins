77a2b25fdef661284b75d8d45140de23b7866001
chelseajenkins <78179632+chelseajenkins@users.noreply.github.com>
2021-02-02 13:42:07 +1300

    madlib

---
+++ Madlib.py
@@ -0,0 +1,43 @@
+# Madlib, This code takes a few random words then puts them in a story.
+name = input("enter a name ")
+he = input("enter a pronoun ")
+animal = input("enter an animal ")
+pet = input("enter a pet name ")
+place = input("enter a place ")
+
+#this is the story of why they didn't do their homework.
+print("Unfortunately", name, "could not complete their homework as")
+print(he, "left it at the", place, "where it was consumed by a", animal, "who was called", pet,"(:")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
