ec56313238fdfdcda021aeb40b9ba6c8ef682cc4
chelseajenkins <78179632+chelseajenkins@users.noreply.github.com>
2021-02-18 13:24:41 +1300

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,12 +3,21 @@
 import random
 num = random.randint(1,100)
 loop = True
+another_loop = True
 while loop == True:
     their_guess = int(input("Guess a number between 1 & 100: "))
-    if their_guess == num:
-        print("That is correct!")
-        loop = False
-    if their_guess <num:
-        print("The number is bigger than", their_guess)
-    elif their_guess >num:
-        print("The number is smaller than", their_guess)
+    if their_guess <=100 and their_guess >=0:
+        another_loop = False
+        if their_guess == num:
+            print("That is correct!")
+            loop = False
+        elif their_guess <num:
+            print("The number is bigger than", their_guess)
+            print()
+        elif their_guess >num:
+            print("The number is smaller than", their_guess)
+    else:
+        while another_loop == True:
+            their_guess = int(input("Guess a number BETWEEN 1 & 100: "))
+            if their_guess <= 100 and their_guess >= 1:
+                another_loop = False
